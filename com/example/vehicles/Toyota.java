ee6511fe7ca092260da40ea0d91dd4b3240a5577
Yoge-2004 <yoge852004@gmail.com>
2025-03-25 07:03:42 +0530

    Delete com/example/vehicles directory

--- com/example/vehicles/Toyota.java
+++
@@ -1,7 +0,0 @@
-package com.example.vehicles;
-
-public class Toyota extends Car {
-    public Toyota(String model, double mileage, int topSpeed, int bootSpace, String engineVariant, int torque) {
-        super("Toyota", model, mileage, topSpeed, bootSpace, engineVariant, torque);
-    }
-}
